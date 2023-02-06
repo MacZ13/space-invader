8b0034c7dbbe02c5fab752d507aee52f9853d250
Bibou <jordan.bouville@edu.esiee-it.com>
2023-02-06 13:24:57 +0100

    crée les grille

---
+++ test.js
@@ -0,0 +1,11 @@
+/* crée les grille */
+
+const app = document.querySelector('.grille')
+
+for (let i = 0; i < 240; i++){
+    let madiv = document.createElement('div');
+    app.appendChild(madiv)
+}
+
+/* Afficher les vaisseau */
+
