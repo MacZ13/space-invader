84235f1ca5ca0b3d57eda5ba8bfc90220614a63f
Bibou <jordan.bouville@edu.esiee-it.com>
2023-02-08 11:14:38 +0100

    [UPDATE] ça marche

--- app.js
+++ app.js
@@ -1,33 +1,51 @@
 /* crée les grille */
 
 const spaceInvader = document.querySelector('.grille')
+let tireurIndex = 202
+let direction = 1
+let width = 20
+let invadersId
+let goBouge = true
 
 for (let i = 1; i < 241; i++){
-    let madiv = document.createElement('div');
-
-    if (i % 20 === 0) {
-        madiv.setAttribute("class", "right_div");
-    }
-
-    if ((i - 1) % 20 === 0) {
-        madiv.setAttribute("class", "left_div");
-    }
-
-    spaceInvader.appendChild(madiv);
+    const maDiv = document.createElement('div');
+    spaceInvader.appendChild(maDiv);
 }
 
-var tableauId = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51];
+const toutesLesDivs = document.querySelectorAll('.grille div');
 
 /* Afficher les vaisseau */
 
+const alienInvaders = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+    20, 21, 22, 23, 24, 25, 26, 27, 28, 29,30, 31,
+    40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51
+]
+
+function draw() {
+    for (let i = 0; i < alienInvaders.length; i++){
+        toutesLesDivs[alienInvaders[i]].classList.add('alien')
+    }
+}
+
+draw()
+
+function remove() {
+    for (let i = 0; i < alienInvaders.length; i++) {
+        toutesLesDivs[alienInvaders[i]].classList.remove('alien')
+    }
+}
+
+toutesLesDivs[230].classList.add('tireur')
+
+
 const tableauGrille = spaceInvader.children; // la variable "tableauGrille" est déclarée qui contient les enfants de l'élément "grille"
 
-tableauGrille[230].classList.add("tireur"); // la classe "tireur" est ajoutée à l'élément de la grille au "tableauI" 230.
-
-
-tableauId.forEach(element => {
+alienInvaders.forEach(element => {
     tableauGrille[element].classList.add("alien");
 });
+
+/* mouvement du tireur */
 
 document.onkeydown = function(event) { // événement de touche enfoncée
     let tireur = document.querySelector(".tireur"); // récupère l'élément de la classe "tireur"
@@ -68,3 +86,36 @@ document.onkeydown = function(event) { // événement de touche enfoncée
         break;
     }
 };
+
+/* mouvement des ennemies */
+
+function moveAlien() {
+    const leftMove = alienInvaders[0] % width === 0
+    const rightMove = alienInvaders[alienInvaders.length - 1] % width === width - 1
+    remove()
+
+    if (rightMove && goBouge) {
+        for (let i = 0; i < alienInvaders.length; i++) {
+            alienInvaders[i] += width + 1
+            direction = -1
+            goBouge = false
+        }
+    }
+
+    if (leftMove && !goBouge) {
+        for (let i = 0; i < alienInvaders.length; i++) {
+            alienInvaders[i] += width - 1
+            direction = 1
+            goBouge = true
+        }
+    }
+
+    for (let i = 0; i < alienInvaders.length; i++) {
+        alienInvaders[i] += direction
+    }
+
+    draw()
+}
+    invadersId = setInterval(moveAlien, 700);
+
+
